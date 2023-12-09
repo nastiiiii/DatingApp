b17922989335d576a6644b7ef4fcb7477f569aa9
nasti <nastia.pyslar@gmail.com>
2023-12-09 20:39:34 +0200

    Angular added

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
 });
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: value => this.users = value,
+      error: err => console.log(err),
+      complete: () => console.log('Request has completed')
+    });
+  }
+}
