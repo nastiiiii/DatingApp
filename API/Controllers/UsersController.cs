53a401764273cee719f701d2fa232473d8a166f4
nasti <nastia.pyslar@gmail.com>
2024-08-13 13:26:33 +0300

    API skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class UsersController : BaseAPI
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+       return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet ("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return user;
+    }
+}
